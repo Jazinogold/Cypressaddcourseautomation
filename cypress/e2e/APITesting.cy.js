6bb062dafee438de1e5a69e5285412b9fc6d7a14
jazinogold <jazinogold@gmail.com>
2023-07-25 22:54:21 +0100

    API plugin

---
+++ cypress/e2e/APITesting.cy.js
@@ -0,0 +1,13 @@
+/// <reference types="cypress" />
+
+describe("This test is to ensure that users can login to the Quales app sucessfully", () => {
+
+  it("Verify user with correct credentials can login into the app", () => {
+
+    
+   
+    
+
+
+  });
+});
