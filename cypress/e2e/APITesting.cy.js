615914f9f82d7f65ffcb73b8b45c52d15dacc34c
jazinogold <jazinogold@gmail.com>
2023-07-28 11:07:53 +0100

    ApiTesting

--- cypress/e2e/APITesting.cy.js
+++ cypress/e2e/APITesting.cy.js
@@ -1,13 +1,87 @@
 /// <reference types="cypress" />
 
-describe("This test is to ensure that users can login to the Quales app sucessfully", () => {
+describe("This test is to validate that CRUD operations can be performed on the Quales Academy website", () => {
+  let courseId = ""; // Declare the courseId variable here
 
-  it("Verify user with correct credentials can login into the app", () => {
+  const baseUrl = "https://api.quales.tech";
+  const path = "/api/courses/";
 
-    
-   
-    
+  it("Verify user can create a course using the POST method", () => {
+    cy.api({
+      method: "POST", 
+      url: baseUrl + path,
+      body: {
+        "title": "Testing Cypress API",
+        "categoryId": 2,
+        "description": "Cypress Api testing",
+        "courseUrl": "https://www.youtube.com/watch?v=8vXoMqWgbQQ",
+        "imageUrl": "https://source.unsplash.com/user/c_v_r/900x800",
+        "location": "online",
+        "isPremium": false,
+      },
+    }).as("createACourseRequest");
 
+    // Fetch the courseId from the response and store it in the variable
+    cy.get("@createACourseRequest").then((res) => { 
+      courseId = res.body.id;
+      const responseBody = res.body;
 
+      // Tests/assertions
+      expect(res.status).to.eq(200);
+      expect(responseBody).to.have.property("id");
+      expect(responseBody).to.have.property("title");
+
+      // Proceed to the second test after courseId is set
+      cy.searchCourse();
+    });
+  });
+
+  // Create a custom Cypress command for the second test
+  Cypress.Commands.add("searchCourse", () => {
+    cy.api({
+      method: "GET", 
+      url: baseUrl + path + courseId,
+    }).as("searchACourseRequest");
+
+    // Fetch the response and perform assertions
+    cy.get("@searchACourseRequest").then((res) => { 
+      const responseBody = res.body;
+
+      // Tests/assertions
+      expect(res.status).to.eq(200);
+      expect(responseBody).to.have.property("id");
+      expect(responseBody).to.have.property("title");
+    });
   });
 });
+
+it("Verify user can create a course using the POST method", () => {
+  cy.api({
+    method: "POST", 
+    url: baseUrl + path,
+    body: {
+      "title": "Testing Cypress API",
+      "categoryId": 2,
+      "description": "Cypress Api testing",
+      "courseUrl": "https://www.youtube.com/watch?v=8vXoMqWgbQQ",
+      "imageUrl": "https://source.unsplash.com/user/c_v_r/900x800",
+      "location": "online",
+      "isPremium": false,
+    },
+  }).as("createACourseRequest");
+
+  // Fetch the courseId from the response and store it in the variable
+  cy.get("@createACourseRequest").then((res) => { 
+    courseId = res.body.id;
+    const responseBody = res.body;
+
+    // Tests/assertions
+    expect(res.status).to.eq(200);
+    expect(responseBody).to.have.property("id");
+    expect(responseBody).to.have.property("title");
+
+    // Proceed to the second test after courseId is set
+    cy.searchCourse();
+  });
+});
+
